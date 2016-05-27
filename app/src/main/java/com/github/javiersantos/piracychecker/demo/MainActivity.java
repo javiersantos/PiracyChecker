9ee5b1f35c381453c4ee8531116dd199c4210d80
Javier Santos <me@javiersantos.me>
2016-05-27 13:32:22 +0200

    Add library demo

---
+++ app/src/main/java/com/github/javiersantos/piracychecker/demo/MainActivity.java
@@ -0,0 +1,84 @@
+package com.github.javiersantos.piracychecker.demo;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.github.javiersantos.piracychecker.PiracyChecker;
+import com.github.javiersantos.piracychecker.PiracyCheckerUtils;
+import com.github.javiersantos.piracychecker.enums.InstallerID;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setImageDrawable(new IconicsDrawable(this).icon(MaterialDesignIconic.Icon.gmi_github).color(Color.WHITE).sizeDp(24));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/javiersantos/PiracyChecker")));
+            }
+        });
+    }
+
+    public void verifySignature(View view) {
+        new PiracyChecker(this)
+                .enableSigningCertificate("478yYkKAQF+KST8y4ATKvHkYibo=")
+                .start();
+    }
+
+    public void readSignature(View view) {
+        Log.e("Signature", PiracyCheckerUtils.getAPKSignature(this));
+        new AlertDialog.Builder(this)
+                .setTitle("APK")
+                .setMessage(PiracyCheckerUtils.getAPKSignature(this))
+                .show();
+    }
+
+    public void verifyInstallerId(View view) {
+        new PiracyChecker(this)
+                .enableInstallerId(InstallerID.GOOGLE_PLAY)
+                .start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        menu.findItem(R.id.action_about).setIcon(new IconicsDrawable(this).icon(MaterialDesignIconic.Icon.gmi_info).color(Color.WHITE).actionBar());
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
