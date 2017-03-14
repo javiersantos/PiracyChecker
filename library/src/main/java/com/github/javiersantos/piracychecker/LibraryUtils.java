a118904ef8bde688ed98cb0f11d82cb5380288ca
Javier Santos <me@javiersantos.me>
2017-03-14 23:49:59 +0100

    Possible fix to emulator check #16

--- library/src/main/java/com/github/javiersantos/piracychecker/LibraryUtils.java
+++ library/src/main/java/com/github/javiersantos/piracychecker/LibraryUtils.java
@@ -121,8 +121,7 @@ class LibraryUtils {
                                     return app;
                                 }
                             }
-                        } catch (Exception ignored3) {
-                        }
+                        } catch (Exception ignored) {}
                     }
                 }
             }
@@ -130,18 +129,109 @@ class LibraryUtils {
         return null;
     }
 
+    /**
+     * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+     * the License. You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+     * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+     * specific language governing permissions and limitations under the License.
+     *
+     * Copyright (C) 2013, Vladislav Gingo Skoumal (http://www.skoumal.net)
+     */
     static boolean isInEmulator() {
-        try {
-            boolean goldfish = getSystemProperty("ro.hardware")
-                    .contains("goldfish");
-            boolean emu = getSystemProperty("ro.kernel.qemu").length() > 0;
-            boolean sdk = getSystemProperty("ro.product.model").equals("sdk");
-            if (emu || goldfish || sdk) {
-                return true;
-            }
-        } catch (Exception ignored) {
+        int ratingCheckEmulator = 0;
+
+        if (Build.PRODUCT.contains("sdk") ||
+                Build.PRODUCT.contains("Andy") ||
+                Build.PRODUCT.contains("ttVM_Hdragon") ||
+                Build.PRODUCT.contains("google_sdk") ||
+                Build.PRODUCT.contains("Droid4X") ||
+                Build.PRODUCT.contains("nox") ||
+                Build.PRODUCT.contains("sdk_x86") ||
+                Build.PRODUCT.contains("sdk_google") ||
+                Build.PRODUCT.contains("vbox86p")) {
+            ratingCheckEmulator++;
         }
-        return false;
+
+        if (Build.MANUFACTURER.equals("unknown") ||
+                Build.MANUFACTURER.equals("Genymotion") ||
+                Build.MANUFACTURER.contains("Andy") ||
+                Build.MANUFACTURER.contains("MIT") ||
+                Build.MANUFACTURER.contains("nox") ||
+                Build.MANUFACTURER.contains("TiantianVM")){
+            ratingCheckEmulator++;
+        }
+
+        if (Build.BRAND.equals("generic") ||
+                Build.BRAND.equals("generic_x86") ||
+                Build.BRAND.equals("TTVM") ||
+                Build.BRAND.contains("Andy")) {
+            ratingCheckEmulator++;
+        }
+
+        if (Build.DEVICE.contains("generic") ||
+                Build.DEVICE.contains("generic_x86") ||
+                Build.DEVICE.contains("Andy") ||
+                Build.DEVICE.contains("ttVM_Hdragon") ||
+                Build.DEVICE.contains("Droid4X") ||
+                Build.DEVICE.contains("nox") ||
+                Build.DEVICE.contains("generic_x86_64") ||
+                Build.DEVICE.contains("vbox86p")) {
+            ratingCheckEmulator++;
+        }
+
+        if (Build.MODEL.equals("sdk") ||
+                Build.MODEL.equals("google_sdk") ||
+                Build.MODEL.contains("Droid4X") ||
+                Build.MODEL.contains("TiantianVM") ||
+                Build.MODEL.contains("Andy") ||
+                Build.MODEL.equals("Android SDK built for x86_64") ||
+                Build.MODEL.equals("Android SDK built for x86")) {
+            ratingCheckEmulator++;
+        }
+
+        if (Build.HARDWARE.equals("goldfish") ||
+                Build.HARDWARE.equals("vbox86") ||
+                Build.HARDWARE.contains("nox") ||
+                Build.HARDWARE.contains("ttVM_x86")) {
+            ratingCheckEmulator++;
+        }
+
+        if (Build.FINGERPRINT.contains("generic/sdk/generic") ||
+                Build.FINGERPRINT.contains("generic_x86/sdk_x86/generic_x86") ||
+                Build.FINGERPRINT.contains("Andy") ||
+                Build.FINGERPRINT.contains("ttVM_Hdragon") ||
+                Build.FINGERPRINT.contains("generic_x86_64") ||
+                Build.FINGERPRINT.contains("generic/google_sdk/generic") ||
+                Build.FINGERPRINT.contains("vbox86p") ||
+                Build.FINGERPRINT.contains("generic/vbox86p/vbox86p")) {
+            ratingCheckEmulator++;
+        }
+
+        try {
+            String opengl = android.opengl.GLES20.glGetString(android.opengl.GLES20.GL_RENDERER);
+            if (opengl != null) {
+                if (opengl.contains("Bluestacks") || opengl.contains("Translator"))
+                    ratingCheckEmulator += 10;
+            }
+        } catch (Exception ignored) {}
+
+        try {
+            File sharedFolder = new File(Environment
+                    .getExternalStorageDirectory().toString()
+                    + File.separatorChar
+                    + "windows"
+                    + File.separatorChar
+                    + "BstSharedFolder");
+
+            if (sharedFolder.exists())
+                ratingCheckEmulator += 10;
+        } catch (Exception ignored) {}
+
+        return ratingCheckEmulator > 3;
     }
 
     static boolean isDebug(Context context) {
@@ -224,13 +314,6 @@ class LibraryUtils {
             }
         }
         return false;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static String getSystemProperty(String name) throws Exception {
-        Class systemPropertyClazz = Class.forName("android.os.SystemProperties");
-        return (String) systemPropertyClazz.getMethod("get", new Class[]{String.class}).invoke
-                (systemPropertyClazz, name);
     }
 
 }
