8a3501f36adf6e7901568dc3b8cf505270d220e7
Javier Santos <me@javiersantos.me>
2017-03-15 14:56:54 +0100

    Update libraries and sample project

--- app/src/main/java/com/github/javiersantos/piracychecker/demo/MainActivity.java
+++ app/src/main/java/com/github/javiersantos/piracychecker/demo/MainActivity.java
@@ -1,18 +1,13 @@
 package com.github.javiersantos.piracychecker.demo;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioGroup;
 
@@ -20,8 +15,6 @@ import com.github.javiersantos.piracychecker.PiracyChecker;
 import com.github.javiersantos.piracychecker.PiracyCheckerUtils;
 import com.github.javiersantos.piracychecker.enums.Display;
 import com.github.javiersantos.piracychecker.enums.InstallerID;
-import com.mikepenz.iconics.IconicsDrawable;
-import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic;
 
 public class MainActivity extends AppCompatActivity {
     private Display piracyCheckerDisplay = Display.DIALOG;
@@ -32,19 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         RadioGroup radioDisplay = (RadioGroup) findViewById(R.id.radio_display);
 
         setSupportActionBar(toolbar);
-
-        fab.setImageDrawable(new IconicsDrawable(this).icon(MaterialDesignIconic.Icon.gmi_github)
-                .color(Color.WHITE).sizeDp(24));
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/javiersantos/PiracyChecker")));
-            }
-        });
 
         radioDisplay.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -63,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
         // Show APK signature
         Log.e("Signature", PiracyCheckerUtils.getAPKSignature(this));
 
+    }
+
+    public void toGithub(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/javiersantos/PiracyChecker")));
     }
 
     public void verifySignature(View view) {
@@ -117,23 +104,4 @@ public class MainActivity extends AppCompatActivity {
                 .start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        menu.findItem(R.id.action_about).setIcon(new IconicsDrawable(this).icon
-                (MaterialDesignIconic.Icon.gmi_info).color(Color.WHITE).actionBar());
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_about:
-                startActivity(new Intent(this, AboutActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
